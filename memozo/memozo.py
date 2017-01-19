d90d650a1749c1016066554ee6bb0745623fcb2f
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2017-01-19 18:04:52 +0900

    remove unnecessary print

--- memozo/memozo.py
+++ memozo/memozo.py
@@ -14,18 +14,13 @@ class Memozo(object):
 
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            print('wrapper called')
-
             if os.path.exists(file_path):
-                print('file exists')
                 with open(file_path, 'r') as f:
                     obj = f.readlines()
                 return obj
 
-            print('run original function')
             obj = func(*args, **kwargs)
 
-            print('saving...')
             with open(file_path, 'w') as f:
                 f.writelines(obj)
 
