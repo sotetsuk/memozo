e1b49d3a477a20728f2745daea179f2a7bde8d1a
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2017-01-19 19:07:50 +0900

    add test_set_name and refactor

--- tests/test_memozo.py
+++ tests/test_memozo.py
@@ -4,19 +4,19 @@ import unittest
 from memozo import Memozo
 
 
-class TestMemozo(unittest.TestCase):
+class TestMemozoCall(unittest.TestCase):
 
     def setUp(self):
         self.base_path = './tests/resources/'
         self.m = Memozo(self.base_path)
+        self.dummy_data = ['This\n', 'is\n', 'a\n', 'test.\n']
 
     def test_call(self):
-        dummy_data = ['This\n', 'is\n', 'a\n', 'test.\n']
 
         @self.m()
         def create_dummy_data():
             """create dummy data"""
-            return dummy_data
+            return self.dummy_data
 
         # testing save and load
         path = os.path.join(self.base_path, 'create_dummy_data')
@@ -24,19 +24,27 @@ class TestMemozo(unittest.TestCase):
             os.remove(path)
         self.assertFalse(os.path.exists(path))
         data = create_dummy_data()
-        self.assertTrue(data == dummy_data)
+        self.assertTrue(data == self.dummy_data)
         self.assertTrue(os.path.exists(path))
-        dummy_data += ['extra line\n']
+        self.dummy_data += ['extra line\n']
         data = create_dummy_data()
-        self.assertFalse(data == dummy_data)
+        self.assertFalse(data == self.dummy_data)
 
     def test_doc_string(self):
-        dummy_data = ['This\n', 'is\n', 'a\n', 'test.\n']
-
-        @self.m(name='call_test')
+        @self.m()
         def create_dummy_data():
             """create dummy data"""
-            return dummy_data
+            return self.dummy_data
 
         # testing doc string
         self.assertTrue(create_dummy_data.__doc__ == "create dummy data")
+
+    def test_set_name(self):
+        name = 'call_test'
+
+        @self.m(name=name)
+        def create_dummy_data():
+            """create dummy data"""
+            return self.dummy_data
+
+        self.assertTrue(os.path.exists(os.path.join(self.base_path, name)))
