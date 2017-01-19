1805e9cc2e2aeb3770b03ebcb05ac189566fa165
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2017-01-19 17:52:40 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='memozo',
+    version="0.1.0",
+    description='Memotize objects to desk easily',
+    author='sotetsuk',
+    url='https://github.com/sotetsuk/memozo',
+    author_email='sotetsu.koyamada@gmail.com',
+    license='MIT',
+    install_requires=[],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
