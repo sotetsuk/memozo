a55e648500219eb2acb51ac3fc2842bcf5d29480
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2017-01-20 12:03:59 +0900

    implement generator method

--- memozo/memozo.py
+++ memozo/memozo.py
@@ -40,3 +40,39 @@ class Memozo(object):
             return _wrapper
 
         return wrapper
+
+    def generator(self, name=None, ext='file'):
+
+        def wrapper(func):
+            _name = func.__name__ if name is None else name
+
+            @functools.wraps(func)
+            def _wrapper(*args, **kwargs):
+                # get cached data path
+                args = utils.get_bound_args(func, *args, **kwargs)
+                args_str = utils.get_args_str(args)
+                sha1 = utils.get_hash(_name, func.__name__, args_str)
+                file_path = os.path.join(self.base_path, "{}_{}.{}".format(_name, sha1, ext))
+
+                # if cached data exists, return generator using cached data
+                if utils.log_exisits(self.base_path, _name, func.__name__, args_str) and os.path.exists(file_path):
+                    def gen_cached_data():
+                        with codecs.open(file_path, 'r', utils.ENCODING) as f:
+                            for line in f:
+                                yield line
+                    return gen_cached_data
+
+                gen = func(*args, **kwargs)
+
+                # if no cached data exists, generator not only yield value but save value at each iteration
+                def generator_with_cache(gen, file_path):
+                    with codecs.open(file_path, 'w', utils.ENCODING) as f:
+                        for e in gen:
+                            f.write(e)
+                            yield e
+
+                return generator_with_cache(gen, file_path)
+
+            return _wrapper
+
+        return wrapper
