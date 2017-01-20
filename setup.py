f669e1254ebaf8da42d03bebb8e686a13bccb280
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2017-01-20 19:12:26 +0900

    release v0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='memozo',
-    version="0.1.1",
+    version="0.1.2",
     description='Memotize objects to desk easily',
     author='sotetsuk',
     url='https://github.com/sotetsuk/memozo',
